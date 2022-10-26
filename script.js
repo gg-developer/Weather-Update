294c2b091269b58000b4edf537fc92310f194524
Zac David <116624949+gg-developer@users.noreply.github.com>
2022-10-26 09:59:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let weather ={
+    "apikey": "bc41ceec266dfe86f748257816dbce71",
+    fetchWeather: function(city){
+        fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q="
+                +city
+                +"&units=metric&appid="
+                +this.apikey
+            )
+                .then(response => response.json())
+                 .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon,description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+       " https://openweathermap.org/img/wn/"+icon+".png";
+       document.querySelector(".temp").innerText = temp + "°C";
+       document.querySelector(".description").innerText = description;
+       document.querySelector(".humadity").innerText = "Humadity: "+ humidity +"%";
+       document.querySelector(".wind").innerText = "Wind speed: "+ speed + "km/h";
+       document.querySelector(".weather").classList.remove("loading");
+       document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1600×900/?"+name+"')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button")
+.addEventListener("click",function(){
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Durgapur");
